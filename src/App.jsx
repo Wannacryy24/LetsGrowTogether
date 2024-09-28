f80baaaa1c8afa08b399a02b9c5c74384b067d66
Mayank Verma <mayankverma.dev@gmail.com>
2024-09-28 12:28:01 +0530

    fng

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { HeaderComponent } from "./Comonent/Header/Header";
 import { LandingPage } from "./Comonent/LandingPage/landingPage";
-
+import { contentData } from "./Data/data";
 export default function App(){
   return (
     <div className="container">
       <HeaderComponent/>
       <LandingPage/>
-  
+      
     </div>
+    
   );
 }
