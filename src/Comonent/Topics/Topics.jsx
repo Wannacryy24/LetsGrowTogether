6e9a021dbe3b65c988bcb7cbbe31a5154994d37d
Sujal0007 <sujalvashisth1010@gmail.com>
2024-09-28 17:51:44 +0530

    added sidebar css and back button

--- src/Comonent/Topics/Topics.jsx
+++ src/Comonent/Topics/Topics.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './Topics.css'
+import { SectionContext } from '../../ContextProvider/SectionContext';
 
 export default function Topics({topics , selectedTopic}) {
+  const{setSelectedTopic , setSelectedSection} = useContext(SectionContext);
+
+  const handleTopicClick = (topic) => {
+    setSelectedTopic(topic);  
+  };
+
   return (
      (
         <div className="main-content">
           <div className="sidebar">
-            <h2>Topics</h2>
+           <div className='side-head'> <h2>Topics</h2><span><button onClick={()=>setSelectedSection(null)}>&larr;</button></span></div>
             <ul>
               {topics.map((topic) => (
                 <li key={topic.id} onClick={() => handleTopicClick(topic)}>
--- src/SectionPage/SectionPage.jsx
+++ src/SectionPage/SectionPage.jsx
@@ -35,9 +35,7 @@ const SectionPage = () => {
   };
 
 
-  const handleTopicClick = (topic) => {
-    setSelectedTopic(topic);  
-  };
+ 
 
 
   return (
