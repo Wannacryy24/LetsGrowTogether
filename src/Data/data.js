dcfd90194a5b20f9267f2c1c08e02286c8213ea9
Sujal0007 <sujalvashisth1010@gmail.com>
2024-09-28 15:17:17 +0530

    added clicked functionality on leran button and on topics

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,101 +1,52 @@
 import { createServer } from 'miragejs';
 
 export function makeServer() {
-  createServer({
+  return createServer({
     routes() {
       this.namespace = 'api';
-      
-      this.get('/sections', () => (
-        [
-          { id: 'html', name: 'HTML' },
-          { id: 'css', name: 'CSS' },
-          { id: 'javascript', name: 'JavaScript' },
-          { id: 'react', name: 'React' }
-        ]
-      ));
 
       
-      this.get('/sections/:sectionId/topics', (schema, request) => {
-        const { sectionId } = request.params;
-        const data = {
-          html: {
-            topics: [
-              'Introduction', 'Elements', 'Attributes', 'Forms', 'Semantics'
-            ],
-            details: {
-              'Introduction': {
-                title: 'HTML Introduction',
-                description: 'HTML is the standard markup language for creating web pages.',
-                points: [
-                  'Used to create the structure of a web page.',
-                  'Composed of elements and tags.'
-                ],
-                image: 'https://www.example.com/html-intro.png'
-              },
-              'Elements': {
-                title: 'HTML Elements',
-                description: 'HTML elements are the building blocks of web pages.',
-                points: ['Examples include <p>, <div>, <h1>, etc.'],
-                image: 'https://www.example.com/html-elements.png'
-              }
-           
-            }
-          },
-          css: {
-            topics: [
-              'Selectors', 'Box Model', 'Flexbox', 'Grid', 'Animations'
-            ],
-            details: {
-              'Selectors': {
-                title: 'CSS Selectors',
-                description: 'Selectors in CSS are used to select the HTML elements you want to style.',
-                points: [
-                  'Different types: Class, ID, Element.',
-                  'Can combine multiple selectors.'
-                ],
-                image: 'https://www.example.com/css-selectors.png'
-              }
-           
-            }
-          },
-          javascript: {
-            topics: [
-              'Introduction', 'DOM Manipulation', 'ES6', 'Functions', 'Async'
-            ],
-            details: {
-              'Introduction': {
-                title: 'JavaScript Introduction',
-                description: 'JavaScript is a versatile programming language for the web.',
-                points: [
-                  'Used for interactivity on web pages.',
-                  'Can manipulate the DOM and handle events.'
-                ],
-                image: 'https://www.example.com/js-intro.png'
-              }
-            
-            }
-          },
-          react: {
-            topics: [
-              'Introduction', 'Components', 'State', 'Props', 'Hooks'
-            ],
-            details: {
-              'Introduction': {
-                title: 'React Introduction',
-                description: 'React is a JavaScript library for building user interfaces.',
-                points: [
-                  'Allows creating reusable UI components.',
-                  'Uses a virtual DOM for performance.'
-                ],
-                image: 'https://www.example.com/react-intro.png'
-              }
-             
-            }
-          }
-        };
+      let sections = [
+        { id: '1', name: 'HTML' },
+        { id: '2', name: 'CSS' },
+        { id: '3', name: 'JavaScript' },
+        { id: '4', name: 'React' },
+      ];
 
-        return data[sectionId] || { topics: [], details: {} };
+  
+      let topics = {
+        '1': [
+          { id: '1-1', title: 'Introduction to HTML', content: 'HTML is the standard markup language for creating web pages.' },
+          { id: '1-2', title: 'HTML Elements', content: 'HTML elements are the building blocks of HTML pages.' },
+          { id: '1-3', title: 'HTML Forms', content: 'HTML forms are used to collect user input.' },
+        ],
+        '2': [
+          { id: '2-1', title: 'Introduction to CSS', content: 'CSS is used to control the style of web documents in a simple and easy way.' },
+          { id: '2-2', title: 'CSS Selectors', content: 'CSS selectors are used to select the HTML elements you want to style.' },
+          { id: '2-3', title: 'CSS Box Model', content: 'The CSS box model is a box that wraps around every HTML element.' },
+        ],
+        '3': [
+          { id: '3-1', title: 'Introduction to JavaScript', content: 'JavaScript is a programming language used to make web pages interactive.' },
+          { id: '3-2', title: 'JavaScript Functions', content: 'Functions are one of the fundamental building blocks in JavaScript.' },
+          { id: '3-3', title: 'JavaScript DOM Manipulation', content: 'JavaScript can change the content of an HTML document.' },
+        ],
+        '4': [
+          { id: '4-1', title: 'Introduction to React', content: 'React is a JavaScript library for building user interfaces.' },
+          { id: '4-2', title: 'React Components', content: 'Components are the building blocks of React applications.' },
+          { id: '4-3', title: 'React State and Props', content: 'State and props are two important concepts in React for handling data.' },
+        ],
+      };
+
+  
+      this.get('/sections', () => {
+        return { sections };
       });
-    }
+
+      
+      this.get('/sections/:id/topics', (schema, request) => {
+        let sectionId = request.params.id;
+        return { topics: topics[sectionId] || [] };
+      });
+    },
   });
 }
