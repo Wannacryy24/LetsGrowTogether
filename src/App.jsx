32ce8a22ab18319204bcac0d8766f2c3eb257783
Mayank Verma <mayankverma.dev@gmail.com>
2024-09-28 11:06:30 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { HeaderComponent } from "./Comonent/Header/Header";
-// import Landing from "./Landing";
 
 export default function App(){
   return (
     <div className="container">
       <HeaderComponent/>
-    {/* <Landing/> */}
     </div>
   );
 }
