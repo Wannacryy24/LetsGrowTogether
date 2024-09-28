694034c19205c1334087dd7b1f5a7a567f749214
Mayank Verma <mayankverma.dev@gmail.com>
2024-09-28 13:38:36 +0530

    removed selectCourse and Added SectionPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { HeaderComponent } from "./Comonent/Header/Header";
 import { LandingPage } from "./Comonent/LandingPage/landingPage";
+import SectionPage from "./SectionPage/SectionPage";
 
 export default function App(){
   return (
     <div className="container">
       <HeaderComponent/>
       <LandingPage/>
-  
+      <SectionPage/>
     </div>
-    
   );
 }
