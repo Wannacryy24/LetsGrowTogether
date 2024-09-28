3dbe9a441142c7210ff71835e839db2e47db7608
himanshurajput8 <rajputhimanshu913@gmail.com>
2024-09-28 11:00:00 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Header from "./Header";
+import HeaderComonent from "./Header";
 import Landing from "./Landing";
 
 export default function App(){
   return (
     <div className="container">
-    <Header/>
+    <HeaderC/>
     <Landing/>
     </div>
   );
--- src/Comonent/Header/Header.jsx
+++ src/Comonent/Header/Header.jsx
@@ -22,4 +22,4 @@ export function HeaderComponent(){
             </div>
         </header>
     )
-}
+}   
