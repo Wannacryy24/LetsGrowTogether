5e3c0cb02a35c509541c0c187dd24e3a0c441509
himanshurajput8 <rajputhimanshu913@gmail.com>
2024-09-28 12:07:18 +0530

    add Select course

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { HeaderComponent } from "./Comonent/Header/Header";
 import { LandingPage } from "./Comonent/LandingPage/landingPage";
+import { SelectCourse } from "./Comonent/HomePage/HomePage";
 
 export default function App(){
   return (
     <div className="container">
       <HeaderComponent/>
       <LandingPage/>
+      <SelectCourse/>
     </div>
   );
 }
