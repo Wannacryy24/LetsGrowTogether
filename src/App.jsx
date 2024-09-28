493ede2711f477a1e14868cadded6979db9d7b28
himanshurajput8 <rajputhimanshu913@gmail.com>
2024-09-28 11:17:10 +0530

    add landingPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { HeaderComponent } from "./Comonent/Header/Header";
+import { LandingPage } from "./Comonent/LandingPage/landingPage";
 
 export default function App(){
   return (
     <div className="container">
       <HeaderComponent/>
+      <LandingPage/>
     </div>
   );
 }
