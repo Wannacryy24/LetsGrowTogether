b3efce2e839bdc2a69b7fca5b7b7da8f9e5003b1
Mayank Verma <mayankverma.dev@gmail.com>
2024-10-02 13:56:42 +0530

    Nested Routing Done previouly ? data was not fetching but used replace() then it is working

--- src/Comonent/Header/Header.jsx
+++ src/Comonent/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext , useState } from 'react';
+import React, { useContext , useEffect, useState } from 'react';
 import Image from '../SharedComponent/Image';
 import Li from '../SharedComponent/Li';
 import './Header.css';
